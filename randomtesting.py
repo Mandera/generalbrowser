56e19b7c6f0b44e65133c26753ae538573183060
Mandera <rickard.abraham@gmail.com>
2021-11-10 17:59:06 +0100

    Added blank test

--- randomtesting.py
+++ randomtesting.py
@@ -10,6 +10,7 @@ Working out protocol for new packages
 Ideas
     Create randomtesting.py in create_blank()
     Create generate.py in each package so that it's easy to generate setup from metadata before committing
+    Create test/test_NAME.py
 
 """
 
