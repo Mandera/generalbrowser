e7fd266f63359775be5d5f4e84263bd38d5aac12
Mandera <rickard.abraham@gmail.com>
2021-11-22 16:08:14 +0100

    Moved over Domain

--- generalbrowser/__init__.py
+++ generalbrowser/__init__.py
@@ -1,1 +1,2 @@
 
+from generalbrowser.domain import Domain
--- randomtesting.py
+++ randomtesting.py
@@ -6,11 +6,8 @@ Working out protocol for new packages
     Open project in pycharm
     Share on GitHub
 
-
 Ideas
-    Create randomtesting.py in create_blank()
     Create generate.py in each package so that it's easy to generate setup from metadata before committing
-    Create test/test_NAME.py
 
 """
 
