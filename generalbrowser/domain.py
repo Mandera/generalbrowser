77ea4fbd2547073beccc6fe00a2decd99993b6d9
Mandera <rickard.abraham@gmail.com>
2021-11-29 19:08:16 +0100

    Added AutuInitBases, render_response, file_to_request and response_to_file

--- generalbrowser/domain.py
+++ generalbrowser/domain.py
@@ -1,11 +1,15 @@
 
 
 from requests import Session
+import webbrowser
+import re
 
 from generalfile import Path
+from generallibrary import AutoInitBases
 
 
-class Domain:
+class Domain(metaclass=AutoInitBases):
+    """ Client methods to talk to API. """
     session_path = Path.get_cache_dir() / "python/session.txt"
     def __init__(self, domain):
         self.domain = domain
@@ -19,16 +23,44 @@ class Domain:
     def store_session(self):
         self.session_path.pickle.write(self.session, overwrite=True)
 
-    def _request(self, method, endpoint, **data):
+    def _request(self, method, endpoint, files=None, **data):
         url = self.url(endpoint=endpoint)
-        result = method(url=url, data=data)
+        result = method(url=url, files=files, data=data)
         self.store_session()
         # print(result.text)
         return result
 
-    def post(self, endpoint, **data):
-        return self._request(method=self.session.post, endpoint=endpoint, **data)
+    def post(self, endpoint, files=None, **data):
+        """ :param endpoint: 
+            :param files: Dict of files or None. """
+        return self._request(method=self.session.post, endpoint=endpoint, files=files, **data)
 
-    def get(self, endpoint, **data):
-        return self._request(method=self.session.get, endpoint=endpoint, **data)
+    def get(self, endpoint, files=None, **data):
+        """ :param endpoint: 
+            :param files: Dict of files or None. """
+        return self._request(method=self.session.get, endpoint=endpoint, files=files, **data)
+
+    @staticmethod
+    def render_response(response):
+        """ Open response in browser. """
+        path = Path.get_cache_dir() / "python/response.htm"  # type: Path
+        path.text.write(response.text, overwrite=True)
+        webbrowser.open(str(path))
+
+    @staticmethod
+    def file_to_request(name, path):
+        """ Return a dictionary of files to be used in request. """
+        path = Path(path)
+        with open(path, "rb") as file:
+            return {name: file}
+
+    @staticmethod
+    def response_to_file(response, path):
+        """ Write a file from response to path. """
+        name = re.findall("filename=(.+)", response.headers['content-disposition'])[0]
+        path = Path(path) / name
+
+        with open(path, "wb") as file:
+            file.write(response.content)
+
 
