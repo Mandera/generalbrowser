96f46d7647c9b1c92fbb0a82664f5fd7d1c4b0fa
Mandera <rickard.abraham@gmail.com>
2021-12-31 11:18:14 +0100

    [CI SKIP] Base is alright, account next

--- generalbrowser/base/server.py
+++ generalbrowser/base/server.py
@@ -1,36 +1,36 @@
 
 from rest_framework.response import Response
-from rest_framework.exceptions import ValidationError
 from django.http import HttpResponse
 from dill import dumps
 
 from generalbrowser.base.client_and_server import _GeneralClientAndServer
-from generallibrary import deco_optional_suppress
 
 
 class GeneralServer(_GeneralClientAndServer):
     """ Server methods to talk to client. """
-    _error = ValidationError
-
     @property
     def data(self):
-        """ :param rest_framework.views.APIView or GeneralServer self: """
+        """ Dictionary of POST values.
+
+            :param rest_framework.views.APIView or GeneralServer self: """
         return self.request.POST.dict()
 
     def extract_data(self, *keys, default=...):
-        """ Returns tuple if any key is given, otherwise a dict. """
+        """ Returns tuple with values of given keys, error for missing keys unless default is specified. """
         if default is Ellipsis:
             return tuple(self.data[key] for key in keys)
         else:
             return tuple(self.data.get(key, default) for key in keys)
 
-    def serialize(self, *models):
+    @staticmethod
+    def serialize(*models):
         """ Todo: Send client models in header instead? That way we can serialize inside success method instead.
-            Convert django models to client models defined in generalmainframe. """
-        client_models = [model.client_model() for model in models]
+            Convert django models to client models. """
+        client_models = [model.create_client_model() for model in models]
         return HttpResponse(dumps(client_models), content_type="application/octet-stream", headers={"testing": 5})
 
-    def success(self, msg=None, files=None, code=None):
+    @staticmethod
+    def success(msg=None, files=None, code=None):
         if files:
             filename, file = next(iter(files.items()))
             response = HttpResponse(file, content_type="application/octet-stream")
@@ -41,20 +41,3 @@ class GeneralServer(_GeneralClientAndServer):
             response.status_code = code
         return response
 
-    def fail(self, msg=None):
-        # return Response(f"Fail: {msg}")
-        raise self._error(msg)
-
-    @deco_optional_suppress(_error)
-    def _scrub(self, value, error, method):
-        """ Raise ValidationError if not passing. """
-        validation = method(value)
-        if not validation:
-            self.fail(msg=validation)
-        return validation
-
-    def scrub_email(self, email, error=True):
-        return self._scrub(value=email, error=error, method=self.validate_email)
-
-    def scrub_password(self, password, error=True):
-        return self._scrub(value=password, error=error, method=self.validate_password)
