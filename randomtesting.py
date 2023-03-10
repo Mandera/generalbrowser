81e0b51b814fc2dbee895e13158d80cad87ac5f2
Mandera <rickard.abraham@gmail.com>
2023-03-10 12:12:02 +0800

    [CI SKIP] Removed local randomtesting files.
    Only have the single one in repos.

--- randomtesting.py
+++
@@ -1,8 +0,0 @@
-
-
-from generalbrowser.assets.account.client import AccountClient
-from generalbrowser.assets.account.clientpage import GeneralSigninPage
-
-
-client = AccountClient(domain="http://127.0.0.1:8000")
-client.create_page()
